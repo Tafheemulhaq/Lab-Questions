ed830fb862c73992ac3bdfbe7a518f93cd783740
Tafheemulhaq <tafheemulhaq2003@gmail.com>
2024-05-30 15:34:52 +0530

    Initial commit

---
+++ Displayname.c
@@ -0,0 +1,9 @@
+//CSE-23-70
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Tafheem Ul Haq\n");
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,14 @@
+//CSE-23-70
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x, y, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    sum=x+y;
+    printf("Sum of %d and %d is %d", x, y, sum);
+    return 0;
+}
